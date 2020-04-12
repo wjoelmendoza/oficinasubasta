0dca65833fcc63aa93cf8fe16b2d0fae6e82bfee
Teresa Salazar <zandratere@gmail.com>
2020-04-11 22:00:04 -0600

    update db_pago

--- src/main/python/recursos/db_pago.py
+++ src/main/python/recursos/db_pago.py
@@ -7,7 +7,13 @@ class DBPago(Conexion):
 
     def crear_pago(self, idCliente, monto):
         sql = """INSERT INTO MEMBRESIA (fecha_pago, fecha_vencimiento, id_cliente,
-        id_tipo_doc, monto) VALUES (%s,%s,%s,%s,%s)"""
+        id_tipo_doc, monto) VALUES """
+
+        if self.base == "mysql":
+            sql += "(%s, %s, %s, %s, %s)"
+        else:
+            sql += "(?, ?, ?, ?, ?)"
+
         fechaP = datetime.now()
         fechaV = fechaP + relativedelta(years=1)
         fechaP = fechaP.strftime("%Y-%m-%d %H:%M:%S")
@@ -18,8 +24,13 @@ class DBPago(Conexion):
         return self._cursor.lastrowid, fechaP
 
     def codigo_pago(self, codigo):
-        sql = """SELECT id_membresia,monto,fecha_pago FROM MEMBRESIA WHERE id_cliente = %s ORDER BY id_membresia
-        DESC LIMIT 1"""
+        sql = """SELECT id_membresia,monto,fecha_pago FROM MEMBRESIA WHERE id_cliente = """
+
+        if self.base == "mysql":
+            sql += "%s ORDER BY id_membresia DESC LIMIT 1"
+        else:
+            sql += "? ORDER BY id_membresia DESC LIMIT 1"
+
         valores = (codigo, )
         self._cursor.execute(sql, valores)
         respuesta = self._cursor.fetchall()
