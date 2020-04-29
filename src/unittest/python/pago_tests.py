8dfbdf5434bf153887a63dabf14800c44a072e88
Walter Mendoza <wjoelmendoza@gmail.com>
2020-04-28 23:43:44 -0600

    test pago

--- src/unittest/python/pago_tests.py
+++ src/unittest/python/pago_tests.py
@@ -62,6 +62,7 @@ class PagoTest(BaseTest):
         client = self.create_app().test_client()
         idcliente = self.crear_afiliado()
         self.crear_pago(idcliente)
+        idcliente = str(idcliente)
 
         datos = {
             "jwt": self.get_token(),
@@ -71,7 +72,9 @@ class PagoTest(BaseTest):
 
         response = client.post("/Pago", data=datos)
         status = response.status
+        print("pre assert")
         self.assertTrue(status.count("406") >= 1)
+        print('post assert')
 
     def test_post(self):
         client = self.create_app().test_client()
