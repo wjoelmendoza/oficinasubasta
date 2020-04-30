83d6bdd89b8df2adc38dfd8bea4bffa291f99541
Walter Mendoza <wjoelmendoza@gmail.com>
2020-04-29 21:16:33 -0600

    correcciones

--- src/unittest/python/afiliado_g_tests.py
+++ src/unittest/python/afiliado_g_tests.py
@@ -1,37 +1,39 @@
-#! /usr/bin/env python3
-from flask import Flask
-from flask_restful import Api
-from recursos.afiliado import AfiliadoG, Afiliado
-from recursos.pago import Pago
-from recursos.pago import PagoG
-from recursos.empleado import Empleado
-from recursos.misc import cargar_llave
+from base import BaseTest
+from recursos.db_afiliado import DBAfiliado
+from recursos.afiliado import AfiliadoG
 
+class AfiliadoGTest(BaseTest):
 
-class Servidor:
-    """
-    Esta clase se encarga de manejar el servidor y ponerlo a funcionar
-    """
-    def __init__(self):
-        """
-        crea el servidor web junto con los recursos que van a estar disponibles
-        """
-        self.app = Flask(__name__)
-        self.api = Api(self.app)
-        self.api.add_resource(Afiliado, '/Afiliado')
-        self.api.add_resource(AfiliadoG, '/Afiliado/<string:jwt>/<int:codigo>/<string:clave>')
-        self.api.add_resource(Pago, '/Pago')
-        self.api.add_resource(PagoG, '/Pago/<string:jwt>/<int:codigo>')
-        self.api.add_resource(Empleado, '/Empleado')
+    def test_get_error406(self):
+        af = AfiliadoG()
+        token = self.get_token()
+        rst, err = af.get(token, None, "123456")
+        self.assertEqual(err, 406)
 
-    def iniciar(self, host='0.0.0.0', port=8080, debug=True):  # pragma: no cover
-        """
-        Se encarga de inicias el servidor web
-        """
-        self.app.run(host=host, port=port, debug=debug)
+    def test_get_error_404(self):
+        client = self.create_app().test_client()
+        jwt = self.get_token()
+        response = client.get(f'/Afiliado/{jwt}/10000/1235')
+        status = response.status
+        self.assertTrue(status.count("404") >= 1)
 
+    def test_get_error_401(self):
+        client = self.create_app().test_client()
+        i = self.crear_afiliado()
+        jwt = self.get_token()
 
-if __name__ == '__main__':  # pragma: no cover
-    cargar_llave()
-    server = Servidor()
-    server.iniciar()
+        response = client.get(f'/Afiliado/{jwt}/{i}/123')
+        status = response.status
+        self.assertTrue(status.count("401") >= 1)
+
+    def test_get(self):
+        client = self.create_app().test_client()
+        i = self.crear_afiliado()
+        jwt = self.get_token()
+        response = client.get(f'/Afiliado/{jwt}/{i}/123456')
+        rst = response.get_json()
+
+        self.assertEqual(i, rst["codigo"])
+        self.assertEqual("test1", rst["nombre"])
+        self.assertEqual(False, rst["vigente"])
+
